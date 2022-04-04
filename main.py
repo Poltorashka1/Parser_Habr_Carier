051eedbbcb96074da9c1d71a9bf6f3d504d143b4
Яфясов Александр <Sanfor228@mail.ru>
2022-04-04 23:50:58 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import csv
 
-
 CSV = 'vacans.csv'
 ua = UserAgent()
 url = 'https://career.habr.com/vacancies?q=Python&type=all'
@@ -13,7 +12,7 @@ headers = {
 }
 
 
-def get_html(url, params=None):
+def get_html(url, params=None, vacansies=None):
     respons = requests.get(url, headers=headers, params=params)
     return respons
 
@@ -26,8 +25,10 @@ def get_content(html):
         vacans.append(
             {
                 'Name': item.find('a', class_='vacancy-card__title-link').get_text(),
-                'Info': item.find('div', class_='vacancy-card__skills').get_text(),
-                'Link': f"https://career.habr.com{item.find('a', class_='vacancy-card__title-link').get('href')}"
+                'Company': item.find('a', class_='link-comp--appearance-dark').get_text(),
+                'Info': f"{item.find('div', class_='vacancy-card__skills').get_text()} • {item.find('div', class_='vacancy-card__meta').get_text()}",
+                'Link': f"https://career.habr.com{item.find('a', class_='vacancy-card__title-link').get('href')}",
+                'Date': item.find('time', class_='basic-date').get_text()
             }
         )
     return vacans
@@ -36,9 +37,9 @@ def get_content(html):
 def save_doc(items, path):
     with open(path, 'w', encoding='UTF-8', newline='') as file:
         write = csv.writer(file, delimiter=';')
-        write.writerow(['Name', 'Info', 'Link'])
+        write.writerow(['Name', 'Company', 'Info', 'Link', 'Date'])
         for item in items:
-            write.writerow([item['Name'], item['Info'], item['Link']])
+            write.writerow([item['Name'], item['Company'], item['Info'], item['Link'], item['Date']])
 
 
 def main():
@@ -49,8 +50,7 @@ def main():
     if html.status_code == 200:
         vacans = []
         for page_ in range(1, page + 1):
-            html = get_html(url, params={'vacancies': page_})
-            print(f'Loading... {page_}')
+            print(f'Loading... {page_} : {page}')
             html = get_html(url, params={'page': page_})
             vacans.extend(get_content(html.text))
             save_doc(vacans, CSV)
